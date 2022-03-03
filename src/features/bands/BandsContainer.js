578d9b3274e790a381e9861fa7787c79685790cf
Karen Olson <karmarie@gmail.com>
2022-03-03 14:47:09 -0600

    commit tree c476d4f060ed7111adf495779c88bf46fcabce99

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,26 @@
 import React from "react";
+import BandInput from "./BandInput";
+
+import { bandAdded } from "./bandsSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const dispatch = useDispatch();
+  const bands = useSelector((state) => state.bands.entities);
+
+  function handleBandSubmit(name) {
+    dispatch(bandAdded(name));
+  }
+
+  const bandList = bands.map((band) => <li key={band}>{band}</li>);
+
+  return (
+    <div>
+      <p>Bands Container</p>
+      <BandInput onBandSubmit={handleBandSubmit} />
+      <ul>{bandList}</ul>
+    </div>
+  );
 }
 
 export default BandsContainer;
